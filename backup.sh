d39b1e2ffc725698a1b652357d2a989d5ad932a1
cbaakman <cbaakman@umcn.nl>
2016-01-13 12:26:01 +0100

    moved path variables to config file

--- backup.sh
+++ backup.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
-cd /data/bin/
-/usr/local/bin/mrs-lock-and-run /data/status/UPDATE_LOCK \
-   ./update-databanks -rRk backup clean 2>&1 | mail -s cmbi4-backup cbaakman@cmbi.ru.nl
+GetConfVar ()
+{
+    sed -n "s/^$1\\s*=\\s*\\(.*\\)/\1/p" databanks.conf
+}
+
+STATSDIR=`GetConfVar STATSDIR`
+DATADIR=`GetConfVar DATADIR`
+
+sudo -u probio /usr/bin/rsync \
+        -rltvC --no-p --chmod=ugo=rwX --exclude=".svn" --exclude="status" --exclude="scratch/www"  --exclude="mongodb" \
+            --exclude="enzyme" --exclude="flags" --exclude="pdb_seqres.txt" --exclude="yasara/" --exclude="whatif/" \
+            --exclude="structure_factors" --exclude="pdb" --exclude="XML-noatom" --exclude="zata" --exclude="whatcheck" \
+            --exclude="mmCIF" --exclude="rcsb" --exclude="pdbechem" --exclude="flags" --exclude="gpcr-models" \
+            --exclude="mrs" --exclude="uniprot" --exclude="blast" --exclude="fasta" --exclude="tmp" --include="*" \
+        $DATADIR /research/cmbi4/data/ 1> $STATSDIR'backup.mirror_log' 2>&1 \
+            || (echo backup >> $STATSDIR'failed_dbs.txt' ; exit 1)
+
+echo Backup done ;
+/usr/bin/touch $STATSDIR'backup.mirror_done'
