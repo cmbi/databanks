00767aa14e8188edd3126d1651095afd7622c67a
Coos Baakman <c.baakman@radboudumc.nl>
2019-03-15 15:40:14 +0100

    test for interpro

---
+++ tests/interactive/run_interpro.py
@@ -0,0 +1,14 @@
+import os
+import sys 
+
+import logging
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+root_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(root_path)
+
+from databanks.fetch import FetchInterproJob
+
+
+job = FetchInterproJob()
+job.run()
