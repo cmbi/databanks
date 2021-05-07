3baf3a82b663c9a4e815163914ab24c12a41ef9b
Coos Baakman <coos.baakman@radboudumc.nl>
2021-05-07 13:35:55 +0000

    mmCIF download script

---
+++ tests/interactive/run_mmcif.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+import logging
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+root_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(root_path)
+
+from databanks.fetch import FetchMmcifJob
+
+fetch_job = FetchMmcifJob()
+fetch_job.run()
